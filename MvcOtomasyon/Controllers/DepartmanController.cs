97563c375c29e620e164a61a46ad8a9d288cc20d
Alperen-orc <82825640+Alperen-orc@users.noreply.github.com>
2023-07-19 16:16:15 +0300

    Departman İşlemleri Ve Personel Tablosu İlişkisi

---
+++ MvcOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOtomasyon.Models.Siniflar;
+
+namespace MvcOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger = c.Departmans.Where(x=>x.Durum==true).ToList();
+            return View(deger);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+		{
+            c.Departmans.Add(d);
+            d.Durum = true;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+		}
+        public ActionResult DepartmanSil(int id)
+		{
+            var dep = c.Departmans.Find(id);
+            dep.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+		}
+        public ActionResult DepartmanGetir(int id)
+		{
+            var dep = c.Departmans.Find(id);
+            return View("DepartmanGetir", dep);
+		}
+        public ActionResult DepartmanGuncelle(Departman d)
+		{
+            var dep = c.Departmans.Find(d.Departmanid);
+            dep.DepartmanAd = d.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+		}
+        public ActionResult DepartmanDetay(int id)
+		{
+            var deger = c.Personels.Where(x => x.Departmanid == id).ToList();
+            var dpt = c.Departmans.Where(x => x.Departmanid == id).Select(y => y.DepartmanAd).FirstOrDefault();
+            ViewBag.dgr = dpt;
+
+            return View(deger);
+		}
+    }
+}
