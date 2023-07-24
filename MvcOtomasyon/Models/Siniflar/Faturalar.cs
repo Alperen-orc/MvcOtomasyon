eb77f8fc7af3dedb3eab2ab5dd243fdbbe8c3625
Alperen-orc <82825640+Alperen-orc@users.noreply.github.com>
2023-07-24 17:03:42 +0300

    Fatura Ekleme İşlemi Ve FaturaKalem Sınıfı

--- MvcOtomasyon/Models/Siniflar/Faturalar.cs
+++ MvcOtomasyon/Models/Siniflar/Faturalar.cs
@@ -11,23 +11,27 @@ namespace MvcOtomasyon.Models.Siniflar
 	{
 		[Key]
 		public int Faturaid { get; set; }
-		[Column(TypeName = "Char")]
+		[Column(TypeName = "VarChar")]
 		[StringLength(1)]
-		public char FaturaSerino { get; set; }
-		[Column(TypeName = "Varchar")]
+		public string FaturaSerino { get; set; }
+
+		[Column(TypeName = "VarChar")]
 		[StringLength(6)]
 		public string FaturaSirano { get; set; }
 		public DateTime Tarih { get; set; }
-		[Column(TypeName = "Varchar")]
+		[Column(TypeName = "VarChar")]
 		[StringLength(60)]
 		public string VergiDairesi { get; set; }
-		public DateTime Saat { get; set; }
-		[Column(TypeName = "Varchar")]
+		[Column(TypeName = "VarChar")]
+		[StringLength(5)]
+		public string Saat { get; set; }
+		[Column(TypeName = "VarChar")]
 		[StringLength(30)]
 		public string TeslimEden { get; set; }
-		[Column(TypeName = "Varchar")]
+		[Column(TypeName = "VarChar")]
 		[StringLength(30)]
 		public string TeslimAlan { get; set; }
+		public decimal Toplam { get; set; }
 
 		public ICollection<FaturaKalem> FaturaKalems { get; set; }
 
